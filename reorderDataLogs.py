45a04a4ace546e7e1978d890871dca7703ae61e6
Tapan <31704654+tapanjella@users.noreply.github.com>
2020-06-16 20:03:56 -0700

    Changed the indentation of reorderDatalogs.py file

--- reorderDataLogs.py
+++ reorderDataLogs.py
@@ -26,26 +26,26 @@ Constraints:
 logs[i] is guaranteed to have an identifier, and a word after the identifier
 
 '''
-    def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        ll = []
-        ld = []
-        
-        for ele in logs:
-            # list of strings for every elememt
-            lele = ele.strip().split(" ")
-            
-            if re.match("^[a-zA-Z]+$", lele[1]):
-                ll.append(lele)
-            else:
-                ld.append(" ".join(lele))
-        
-        print(ll)
-        
-        
-        ll = [" ".join(i) for i in sorted(ll, key = lambda x: x[1:])]
-        
-        
-        
-        print(ll)
-        
-        return ll + ld
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+  ll = []
+  ld = []
+
+  for ele in logs:
+      # list of strings for every elememt
+      lele = ele.strip().split(" ")
+
+      if re.match("^[a-zA-Z]+$", lele[1]):
+          ll.append(lele)
+      else:
+          ld.append(" ".join(lele))
+
+  print(ll)
+
+
+  ll = [" ".join(i) for i in sorted(ll, key = lambda x: x[1:])]
+
+
+
+  print(ll)
+
+  return ll + ld
