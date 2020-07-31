1ee42e5adb805d0edcd8413fb2052ad39b096fba
Tapan <31704654+tapanjella@users.noreply.github.com>
2020-07-30 18:14:10 -0700

    Updated indentation

--- AddStrings.py
+++ AddStrings.py
@@ -10,21 +10,21 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 
 '''
 
-    def addStrings(self, num1: str, num2: str) -> str:
-        #123
-        # 98
-        #221 
-        res = ""
-        carry = 0
-        i = len(num1) - 1
-        j = len(num2) - 1
-        while i >=0 or j >=0 or carry >0:
-            if i>=0:
-                carry += int(num1[i])
-                i -= 1
-            if j>=0:
-                carry += int(num2[j])
-                j -= 1
-            res += str(carry%10)
-            carry = carry//10
-        return res[::-1]
+def addStrings(self, num1: str, num2: str) -> str:
+    #123
+    # 98
+    #221 
+    res = ""
+    carry = 0
+    i = len(num1) - 1
+    j = len(num2) - 1
+    while i >=0 or j >=0 or carry >0:
+        if i>=0:
+            carry += int(num1[i])
+            i -= 1
+        if j>=0:
+            carry += int(num2[j])
+            j -= 1
+        res += str(carry%10)
+        carry = carry//10
+    return res[::-1]
