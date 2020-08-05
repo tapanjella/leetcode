1d6e95b95e24a981e7188b14202eb0f128f80c26
Tapan Kumar Jella <you@example.com>
2020-08-05 08:02:05 -0700

    sample comment

---
+++ gitpush.sh
@@ -0,0 +1,15 @@
+#Simple script to automate the process of git add, commit, push
+ 
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
+read
