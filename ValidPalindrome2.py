f100fd316efa3b85e4b1838ebeb3bd6222ccc4cd
Tapan <31704654+tapanjella@users.noreply.github.com>
2020-08-05 07:49:50 -0700

    Updated Indentation

--- ValidPalindrome2.py
+++ ValidPalindrome2.py
@@ -12,13 +12,13 @@ Output: True
 Explanation: You could delete the character 'c'.
 
 '''
-    def validPalindrome(self, s: str) -> bool:
-        l = 0
-        r = len(s) - 1
-        while l<r:
-            if s[l] == s[r]:
-                l += 1
-                r -= 1
-            else:
-                return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
-        return True
+def validPalindrome(self, s: str) -> bool:
+    l = 0
+    r = len(s) - 1
+    while l<r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
+    return True
