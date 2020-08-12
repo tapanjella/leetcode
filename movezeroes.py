abdf672e7e0d932dc8b5d9afcb9fc2c9cdc95a6b
Tapan Kumar Jella <you@example.com>
2020-08-12 09:15:43 -0700

    another solution for movezeroes.py

--- movezeroes.py
+++ movezeroes.py
@@ -14,3 +14,17 @@ def moveZeroes(self, nums: List[int]):
         arr.append(0)
     for i in range(len(arr)):
         nums[i] = arr[i]
+    #Alternate Solution
+
+    '''
+        def moveZeroes(self, nums: List[int]) -> None:
+            """
+        Do not return anything, modify nums in-place instead.
+        """
+        nextIndex = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[nextIndex] = nums[nextIndex],nums[i]
+                nextIndex += 1
+        return nums
+    '''
