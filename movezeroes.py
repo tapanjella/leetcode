6ad47cb47242223d3a97b80a7690549bc41a772f
Tapan Kumar Jella <you@example.com>
2020-08-12 08:44:57 -0700

    movezeroes

---
+++ movezeroes.py
@@ -0,0 +1,16 @@
+def moveZeroes(self, nums: List[int]):
+    '''
+    Do not return anything, modify nums in-place instead.
+
+    '''
+    arr = []
+    count_zero = 0
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            count_zero += 1
+        else:
+            arr.append(nums[i])
+    for i in range(count_zero):
+        arr.append(0)
+    for i in range(len(arr)):
+        nums[i] = arr[i]
